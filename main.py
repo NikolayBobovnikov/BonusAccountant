686aa3148a9e93425908743db2e013ec5b37fea3
Nikolay Bobovnikov <nbobovnikov@softech.ru>
2018-11-04 17:34:40 +0300

    add python script with gui (tkinter) and sql db (sqlite)

---
+++ main.py
@@ -0,0 +1,15 @@
+import sqlite3
+import tkinter
+
+
+# database connection
+db = sqlite3.connect('bonus_database.db')
+
+# sql queries for creating tables
+
+table_users = "create table "
+
+
+top = tkinter.Tk()
+# Code to add widgets will go here...
+top.mainloop()
